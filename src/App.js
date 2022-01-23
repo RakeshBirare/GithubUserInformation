f7f9339966adb1e04a1bf3f562221335bc677f9d
Rakesh <rakeshbirare9@gmail.com>
2022-01-23 13:09:11 +0530

    Ready For github CI CD

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 //react-router
-import { BrowserRouter as Router,Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 
 //tostify
 import { ToastContainer } from 'react-toastify';
--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -4,7 +4,7 @@ import {Card, CardBody} from "reactstrap";
 const UserCard = ({user}) => {
     return (
         <Card className = "text-center mt-3 mb-4">
-        <img src={user.avatar_url} className="img-thumbnail" />
+        <img src={user.avatar_url} className="img-thumbnail" alt="avatar" />
         <CardBody>
         <div className="text-primary"><a href={user.html_url} target="_blank"><h1>{user.name}</h1></a></div>
         <div className="text-primary"><h3>{user.location}</h3></div>
